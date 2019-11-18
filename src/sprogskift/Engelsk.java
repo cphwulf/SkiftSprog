476d7f7980cc7f252da80c18d79c512db37dfdb3
Thor <thor@cph-r90bktce.efif.dk>
2019-11-18 09:48:04 +0100

    trash

---
+++ src/sprogskift/Engelsk.java
@@ -0,0 +1,18 @@
+package sprogskift;
+public class Engelsk {
+		public void printMenu() {
+		System.out.println("Welcome ");
+		System.out.println("1) show dogs");
+		System.out.println("2) choose dog");
+		System.out.println("3) search by name");
+		System.out.println("4) chang language");
+		System.out.println("9) exit ");
+	}
+
+	public void printLang() {
+		System.out.println("Change language ");
+		System.out.println("1) Danish");
+		System.out.println("2) English");
+	}
+	
+}
--- src/sprogskift/SprogSkift.java
+++ src/sprogskift/SprogSkift.java
@@ -1,23 +1,53 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package sprogskift;
+
+import java.util.Scanner;
 
 /**
  *
  * @author thor
  */
 public class SprogSkift {
-
+	
 	/**
 	 * @param args the command line arguments
 	 */
+	//static Dansk sprog;
+	static Engelsk sprog;
+	static Scanner sc;
 	public static void main(String[] args) {
-		Dansk sprog = new Dansk();
+		//sprog = new Dansk();
+		sprog = new Engelsk();
 		sprog.printMenu();
+		int choice = 0;
+		Scanner sc = new Scanner(System.in);
+		while (choice != 9) {
+			choice = sc.nextInt();
+			switch(choice) {
+				case 4:
+					changeLanguage();
+					break;
+					
+				default:
+					choice = 9;
+					break;
+					
+			}
+		}
 		// print en menu
+	}
+
+	public static void changeLanguage() {
+		sprog.printLang();
+		int langchoice = sc.nextInt();
+		switch(langchoice) {
+			case 1:sprog = new Dansk();break;
+			case 2:sprog = new Engelsk();break;
+		}
 	}
 	
 }
